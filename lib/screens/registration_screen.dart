0bedf48bc7dbafa2a92b63d8f2ded5bcb3853fb0
jesther-ai <jesthersilvestre@yahoo.com>
2022-03-15 20:39:47 +0800

    [update]

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,3 +1,4 @@
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_responsive_ui/widgets/input_textfield.dart';
 import 'package:flutter_netflix_responsive_ui/widgets/primary_button.dart';
@@ -47,7 +48,7 @@ class Registration extends StatelessWidget {
                     height: 55,
                     keyboardType: TextInputType.text,
                     floatingLabelBehavior: FloatingLabelBehavior.auto,
-                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                    padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
                   ),
                   const SizedBox(height: 20),
                   InputTextField(
@@ -56,7 +57,7 @@ class Registration extends StatelessWidget {
                     height: 55,
                     keyboardType: TextInputType.text,
                     floatingLabelBehavior: FloatingLabelBehavior.auto,
-                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                    padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
                   ),
                   const SizedBox(height: 20),
                   InputTextField(
@@ -65,7 +66,7 @@ class Registration extends StatelessWidget {
                     height: 55,
                     keyboardType: TextInputType.text,
                     floatingLabelBehavior: FloatingLabelBehavior.auto,
-                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                    padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
                   ),
                   const SizedBox(height: 20),
                   InputTextField(
@@ -75,6 +76,11 @@ class Registration extends StatelessWidget {
                     keyboardType: TextInputType.numberWithOptions(decimal: true, signed: true),
                     floatingLabelBehavior: FloatingLabelBehavior.auto,
                     padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                    prefixIconPadding: const EdgeInsets.only(top: 10, right: 0, left: 0),
+                    prefixIcon: GestureDetector(
+                      onTap: () => countryCodeDialog(context: context),
+                      child: Text('+63'),
+                    ),
                   ),
                   const SizedBox(height: 20),
                   InputTextField(
@@ -84,7 +90,7 @@ class Registration extends StatelessWidget {
                     keyboardType: TextInputType.text,
                     obscureText: true,
                     floatingLabelBehavior: FloatingLabelBehavior.auto,
-                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                    padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
                   ),
                   const SizedBox(height: 20),
                   InputTextField(
@@ -94,7 +100,7 @@ class Registration extends StatelessWidget {
                     keyboardType: TextInputType.text,
                     obscureText: true,
                     floatingLabelBehavior: FloatingLabelBehavior.auto,
-                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                    padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
                   ),
                   const SizedBox(height: 20),
                   PrimaryButton(
@@ -114,6 +120,54 @@ class Registration extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  countryCodeDialog({required BuildContext context}) {
+    Future.delayed(const Duration(milliseconds: 2000), () => Navigator.of(context).pop());
+    showGeneralDialog(
+      barrierDismissible: false,
+      context: context,
+      pageBuilder: (context, animation1, animation2) => const SizedBox(),
+      transitionBuilder: (context, anim1, anim2, child) {
+        final curvedValue = Curves.easeInOutBack.transform(anim1.value) - 1.0;
+
+        return Transform(
+          transform: Matrix4.translationValues(0.0, curvedValue * 200, 0.0),
+          child: Opacity(
+            opacity: anim1.value,
+            child: AlertDialog(
+              actionsAlignment: MainAxisAlignment.center,
+              contentPadding: const EdgeInsets.only(left: 15, top: 15, right: 15, bottom: 15),
+              actionsPadding: const EdgeInsets.only(bottom: 10),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
+              ),
+              backgroundColor: Colors.white,
+              content: AnimatedContainer(
+                duration: const Duration(milliseconds: 50),
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                color: Colors.white,
+                child: CountryCodePicker(
+                  onChanged: (value) {
+                    print(value);
+                    Navigator.of(context)
+                      ..pop()
+                      ..pop();
+                  },
+                  initialSelection: 'PH',
+                  showCountryOnly: true,
+                  showOnlyCountryWhenClosed: true,
+                  alignLeft: false,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
