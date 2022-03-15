1e8ab7e5fca11b22015049d74b93bf023b68c00b
jesther-ai <jesthersilvestre@yahoo.com>
2022-03-15 19:07:44 +0800

    [update]

--- lib/screens/registration_dart.dart
+++ lib/screens/registration_dart.dart
@@ -14,7 +14,27 @@ class Registration extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: new Text("Hello background"),
+          child: Container(
+            height: 250,
+            width: double.infinity,
+            margin: const EdgeInsets.only(left: 20, right: 20),
+            decoration: BoxDecoration(
+              color: Colors.black45,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/netflixLogo.png',
+                  height: 150,
+                  width: 150,
+                ),
+                Text("Hello background"),
+              ],
+            ),
+          ),
         ),
       ),
     );
