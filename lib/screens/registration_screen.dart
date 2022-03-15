95a25c98241403bb80691c5e56f5a4bfdad7c3d9
jesther-ai <jesthersilvestre@yahoo.com>
2022-03-15 20:10:28 +0800

    [update]

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -4,6 +4,11 @@ import 'package:flutter_netflix_responsive_ui/widgets/input_textfield.dart';
 class Registration extends StatelessWidget {
   Registration({Key? key}) : super(key: key);
   final TextEditingController firstName = TextEditingController();
+  final TextEditingController lastName = TextEditingController();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController mobileNumber = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  final TextEditingController confirmPassword = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,33 +20,83 @@ class Registration extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Container(
-            height: 250,
-            width: double.infinity,
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            decoration: BoxDecoration(
-              color: Colors.black54,
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
+          child: SingleChildScrollView(
+            child: Container(
+              height: 750,
+              width: double.infinity,
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
               ),
-            ),
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/images/netflixLogo.png',
-                  height: 150,
-                  width: 150,
-                ),
-                InputTextField(
-                  controller: firstName,
-                  hintText: 'First Name',
-                  height: 55,
-                  keyboardType: TextInputType.visiblePassword,
-                  floatingLabelBehavior: FloatingLabelBehavior.auto,
-                  padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
-                ),
-              ],
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/images/netflixLogo.png',
+                    height: 150,
+                    width: 150,
+                  ),
+                  const SizedBox(height: 25),
+                  InputTextField(
+                    controller: firstName,
+                    hintText: 'First Name',
+                    height: 55,
+                    keyboardType: TextInputType.text,
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                  ),
+                  const SizedBox(height: 20),
+                  InputTextField(
+                    controller: lastName,
+                    hintText: 'Last Name',
+                    height: 55,
+                    keyboardType: TextInputType.text,
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                  ),
+                  const SizedBox(height: 20),
+                  InputTextField(
+                    controller: email,
+                    hintText: 'E-mail',
+                    height: 55,
+                    keyboardType: TextInputType.text,
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                  ),
+                  const SizedBox(height: 20),
+                  InputTextField(
+                    controller: mobileNumber,
+                    hintText: 'Mobile Number',
+                    height: 55,
+                    keyboardType: TextInputType.numberWithOptions(decimal: true, signed: true),
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                  ),
+                  const SizedBox(height: 20),
+                  InputTextField(
+                    controller: password,
+                    hintText: 'Passwords',
+                    height: 55,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                  ),
+                  const SizedBox(height: 20),
+                  InputTextField(
+                    controller: confirmPassword,
+                    hintText: 'Confirm Password',
+                    height: 55,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
