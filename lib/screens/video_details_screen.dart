6bd4322c789e2db10a5b04c4fa20405b060cec0e
jesther-ai <jesthersilvestre@yahoo.com>
2022-03-28 09:14:31 +0800

    [minor update]

--- lib/screens/video_details_screen.dart
+++ lib/screens/video_details_screen.dart
@@ -1,12 +1,12 @@
 import 'package:aq_prime/models/content_model.dart';
 import 'package:aq_prime/widgets/app_bar_video_details.dart';
+import 'package:aq_prime/widgets/genre_card.dart';
 import 'package:aq_prime/widgets/movie_info_card.dart';
 import 'package:aq_prime/widgets/profile_card.dart';
 import 'package:aq_prime/widgets/section_title_card.dart';
 import 'package:aq_prime/widgets/sliver_appbar.dart';
 import 'package:aq_prime/widgets/title_description_movie_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class VideoDetailsPage extends StatelessWidget {
   const VideoDetailsPage({Key? key}) : super(key: key);
@@ -102,50 +102,5 @@ class VideoDetailsPage extends StatelessWidget {
     } else {
       return '';
     }
-  }
-}
-
-class GenreCard extends StatelessWidget {
-  const GenreCard({
-    required this.isLast,
-    required this.genreName,
-    Key? key,
-  }) : super(key: key);
-  final bool isLast;
-  final String genreName;
-  @override
-  Widget build(BuildContext context) {
-    return AnimationConfiguration.staggeredList(
-      position: 1,
-      duration: const Duration(milliseconds: 500),
-      child: FadeInAnimation(
-        child: SlideAnimation(
-          horizontalOffset: 100,
-          child: Container(
-            width: 220,
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(30, 31, 65, 1),
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-            ),
-            margin: EdgeInsets.only(right: isLast ? 0 : 20, bottom: 0),
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Center(
-              child: Text(
-                genreName,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 19,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
