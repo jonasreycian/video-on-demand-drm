5e37f5a0564832498075bd1c92bb5ba3cb012945
jesther-ai <jesthersilvestre@yahoo.com>
2022-04-17 15:57:04 +0800

    forgot password API

--- lib/providers/forgot_password_provider.dart
+++ lib/providers/forgot_password_provider.dart
@@ -1,33 +1,58 @@
 import 'package:country_code_picker/country_code.dart';
 import 'package:flutter/material.dart';
+import 'package:aq_prime/utilities/api_request.dart';
 
 class ForgotPasswordProvider with ChangeNotifier {
+  bool _isObscure = true;
+  bool _isObscureConfirm = true;
+
   bool _isSuccessOtp = false;
   bool _isSuccess = false;
   bool _isLoading = true;
-  CountryCode _countryCode = CountryCode(code: 'PH', dialCode: '+63', flagUri: 'flags/ph.png', name: 'Pilipinas');
+  String? _password;
+  String? _confirmPassword;
+
   String _hintText = 'Email';
+  String _submitName = 'Mail OTP';
+  CountryCode _countryCode = CountryCode(code: 'PH', dialCode: '+63', flagUri: 'flags/ph.png', name: 'Pilipinas');
+  String? _email;
   TextInputType _textInputType = TextInputType.text;
   String _selectedWidgetCard = 'Reset via Email';
   final List<String> _choices = ['Reset via Mobile Number', 'Reset via Email'];
-  String _message = 'Please Wait...';
+  String? _message;
   //getter
+  bool get isObscure => _isObscure;
+  bool get isObscureConfirm => _isObscureConfirm;
   String get hintText => _hintText;
+  String get submitName => _submitName;
+  String? get email => _email;
   CountryCode get countryCode => _countryCode;
   TextInputType get textInputType => _textInputType;
-  String get message => _message;
+  String? get message => _message;
   bool get isSuccess => _isSuccess;
   bool get isSuccessOtp => _isSuccessOtp;
   bool get isLoading => _isLoading;
   String get selectedWidgetCard => _selectedWidgetCard;
   List<String> get choices => [..._choices];
+  String? get password => _password;
+  String? get confirmPassword => _confirmPassword;
   //setter
-  setCountryCode(value) {
-    _countryCode = value;
+  setIsObscure(bool value) {
+    _isObscure = value;
     notifyListeners();
   }
 
-  setStateHAHA() {
+  setIsObscureConfirm(bool value) {
+    _isObscureConfirm = value;
+    notifyListeners();
+  }
+
+  setEmail(value) => _email = value;
+  setPassword(String value) => _password = value;
+  setConfirmPassword(String value) => _confirmPassword = value;
+
+  setCountryCode(CountryCode value) {
+    _countryCode = value;
     notifyListeners();
   }
 
@@ -35,41 +60,43 @@ class ForgotPasswordProvider with ChangeNotifier {
     _selectedWidgetCard = value;
     if (_selectedWidgetCard == _choices[0]) {
       _hintText = 'Mobile number';
+      _submitName = 'SMS OTP';
       _textInputType = TextInputType.number;
     }
     if (_selectedWidgetCard == _choices[1]) {
       _hintText = 'Email';
+      _submitName = 'Mail OTP';
       _textInputType = TextInputType.text;
     }
     notifyListeners();
   }
 
-  sendEmailOrNumber(value) {
+  sendEmailOrNumber() {
     reset();
-    Future.delayed(const Duration(milliseconds: 2000), () {
-      _isSuccess = true;
-      if (RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value) ||
-          value.length >= 8) {
-        if (_hintText == 'Mobile number') _message = 'Please check your SMS inbox';
-        if (_hintText == 'Email') _message = 'Please check your Email';
-        _isSuccess = true;
-        _isLoading = false;
-        notifyListeners();
-      }
-      if (_hintText == 'Email' &&
-          !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)) {
-        _message = 'Invalid Email Address';
-        _isSuccess = false;
-        _isLoading = false;
-        notifyListeners();
-      }
-      if (_hintText == 'Mobile number' && value.length < 8) {
-        _message = 'Phone Number must be\n8 characters above';
-        _isSuccess = false;
-        _isLoading = false;
-        notifyListeners();
-      }
-    });
+    if (RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(_email!) && (_password != null && _password!.length < 8) && (_confirmPassword != null && _confirmPassword!.length < 8)) {
+      Map<String, dynamic> body = {'email': _email, 'password': _password, 'password_confirmation': _confirmPassword};
+      API().request(parameter: body, endPoint: '/v1/reset-password').then((value) {
+        if (value['exception'] == null) {
+          if (_email == 'Mobile number') _message = 'Please check your SMS inbox';
+          if (_email == 'Email') _message = 'Please check your Email';
+          _isSuccess = true;
+          _isLoading = false;
+          notifyListeners();
+        }
+      });
+    }
+    if (_email == 'Email' && !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(_email!)) {
+      _message = 'Invalid Email Address';
+      _isSuccess = false;
+      _isLoading = false;
+      notifyListeners();
+    }
+    if (_password!.length < 8 && _confirmPassword!.length < 8) {
+      _message = 'Password must be\n8 characters above';
+      _isSuccess = false;
+      _isLoading = false;
+      notifyListeners();
+    }
   }
 
   sendOTP(otpValue) {
@@ -83,9 +110,9 @@ class ForgotPasswordProvider with ChangeNotifier {
     _isSuccessOtp = false;
     _isLoading = true;
     // _countryCode = CountryCode(code: 'PH', dialCode: '+63', flagUri: 'flags/ph.png', name: 'Pilipinas');
-    // _hintText = 'Email';
+    // _email = 'Email';
     // _textInputType = TextInputType.text;
     // _selectedWidgetCard = 'Reset via Email';
-    _message = 'Please Wait...';
+    _message = null;
   }
 }
--- lib/widgets/reset_password_email_card.dart
+++ lib/widgets/reset_password_email_card.dart
@@ -1,3 +1,4 @@
+import 'package:aq_prime/utilities/hex_color.dart';
 import 'package:flutter/material.dart';
 import 'package:aq_prime/providers/forgot_password_provider.dart';
 import 'package:aq_prime/utilities/dialog.dart';
@@ -10,7 +11,8 @@ class ResetPasswordCardEmail extends StatelessWidget {
   ResetPasswordCardEmail({
     Key? key,
   }) : super(key: key);
-
+  final TextEditingController password = TextEditingController();
+  final TextEditingController confirmPassword = TextEditingController();
   final TextEditingController emailNumber = TextEditingController();
   final FocusNode emailNumberFocus = FocusNode();
   @override
@@ -18,7 +20,7 @@ class ResetPasswordCardEmail extends StatelessWidget {
     return Consumer<ForgotPasswordProvider>(builder: (context, value, child) {
       Future.delayed(const Duration(milliseconds: 100), () {
         if (!value.isLoading && !value.isSuccess) {
-          generalDialog(context: context, message: value.message, isAutoClose: true, isLoading: value.isSuccess);
+          generalDialog(context: context, message: value.message ?? '', isAutoClose: true, isLoading: value.isSuccess);
           value.reset();
         }
       });
@@ -29,6 +31,7 @@ class ResetPasswordCardEmail extends StatelessWidget {
       //     value.reset();
       //   }
       // });
+      emailNumber.text = value.email ?? '';
       return Column(
         children: [
           Image.asset(
@@ -47,6 +50,7 @@ class ResetPasswordCardEmail extends StatelessWidget {
           ),
           const SizedBox(height: 20),
           InputTextField(
+            onChanged: (p0) => value.setEmail(p0),
             focusNode: emailNumberFocus,
             controller: emailNumber,
             hintText: value.hintText,
@@ -56,20 +60,53 @@ class ResetPasswordCardEmail extends StatelessWidget {
             padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
           ),
           const SizedBox(height: 20),
+          InputTextField(
+            controller: password,
+            hintText: 'Password',
+            height: 55,
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            floatingLabelBehavior: FloatingLabelBehavior.auto,
+            padding: const EdgeInsets.only(left: 20, top: 8, bottom: 8),
+            suffixIconPadding: const EdgeInsets.only(top: 3, bottom: 10, right: 10),
+            suffixIcon: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                value.isObscure ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                color: HexColor('#BEBBBB'),
+                size: 20,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          InputTextField(
+            controller: confirmPassword,
+            hintText: 'Confirm Password',
+            height: 55,
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            floatingLabelBehavior: FloatingLabelBehavior.auto,
+            padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+            suffixIconPadding: const EdgeInsets.only(top: 3, bottom: 10, right: 10),
+            suffixIcon: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                value.isObscureConfirm ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                color: HexColor('#BEBBBB'),
+                size: 20,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
           PrimaryButton(
-            label: 'Mail OTP',
+            label: value.submitName,
             width: double.infinity,
             height: 50,
             action: () {
               if (emailNumber.text.isNotEmpty) {
-                value.sendEmailOrNumber(emailNumber.text);
+                value.sendEmailOrNumber();
                 emailNumberFocus.unfocus();
-                generalDialog(
-                  context: context,
-                  message: value.message,
-                  isAutoClose: true,
-                  isLoading: true,
-                );
+                forgotPasswordDialog(context);
               } else {
                 generalDialog(
                   context: context,
@@ -83,5 +120,81 @@ class ResetPasswordCardEmail extends StatelessWidget {
         ],
       );
     });
+  }
+
+  forgotPasswordDialog(context) {
+    showGeneralDialog(
+      barrierDismissible: false,
+      context: context,
+      pageBuilder: (context, animation1, animation2) {
+        return const SizedBox();
+      },
+      transitionBuilder: (context, anim1, anim2, child) {
+        final curvedValue = Curves.easeInOutBack.transform(anim1.value) - 1.0;
+
+        return Transform(
+          transform: Matrix4.translationValues(0.0, curvedValue * 200, 0.0),
+          child: Opacity(
+            opacity: anim1.value,
+            child: AlertDialog(
+              actionsAlignment: MainAxisAlignment.center,
+              contentPadding: const EdgeInsets.all(15),
+              actionsPadding: const EdgeInsets.only(bottom: 10),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15.0),
+                ),
+              ),
+              backgroundColor: Colors.black87,
+              content: AnimatedContainer(
+                duration: const Duration(milliseconds: 50),
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                color: Colors.transparent,
+                child: Consumer<ForgotPasswordProvider>(builder: (context, value, child) {
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          value.message ?? 'Please Wait...',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      value.isLoading || value.isSuccess
+                          ? SizedBox(
+                              width: 25,
+                              height: 25,
+                              child: CircularProgressIndicator(
+                                color: Colors.red,
+                                strokeWidth: 2.5,
+                              ),
+                            )
+                          : SizedBox(
+                              width: 25,
+                              height: 25,
+                              child: Icon(
+                                Icons.close,
+                                color: Colors.red,
+                              ),
+                            ),
+                    ],
+                  );
+                }),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
