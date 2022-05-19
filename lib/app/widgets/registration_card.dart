8b2362b9244159fcaf565d8f7e642a2f71cbd2fa
jesther-ai <jesthersilvestre@yahoo.com>
2022-05-19 15:06:26 +0800

    added terms and condition checkbox

--- lib/app/widgets/registration_card.dart
+++ lib/app/widgets/registration_card.dart
@@ -1,6 +1,7 @@
 import 'package:aq_prime/app/providers/registration_provider.dart';
 import 'package:aq_prime/app/screens/nav_screen.dart';
 import 'package:aq_prime/app/widgets/calendar_card.dart';
+import 'package:aq_prime/app/widgets/check_box.dart';
 import 'package:aq_prime/app/widgets/input_textfield.dart';
 import 'package:aq_prime/app/widgets/primary_button.dart';
 import 'package:aq_prime/device/utils/dialog.dart';
@@ -38,149 +39,158 @@ class RegistrationCard extends StatelessWidget {
             ..pushReplacementNamed(NavScreen.routeName);
         }
       });
-      return Column(
-        children: [
-          Image.asset(
-            'assets/images/AQ_PRIME_LOGO_2.png',
-            height: 130,
-            width: double.infinity,
-          ),
-          const SizedBox(height: 25),
-          InputTextField(
-            controller: firstName,
-            hintText: 'First Name',
-            height: 55,
-            keyboardType: TextInputType.text,
-            floatingLabelBehavior: FloatingLabelBehavior.auto,
-            padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
-          ),
-          const SizedBox(height: 20),
-          InputTextField(
-            controller: lastName,
-            hintText: 'Last Name',
-            height: 55,
-            keyboardType: TextInputType.text,
-            floatingLabelBehavior: FloatingLabelBehavior.auto,
-            padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
-          ),
-          const SizedBox(height: 20),
-          InputTextField(
-            controller: email,
-            hintText: 'E-mail',
-            height: 55,
-            keyboardType: TextInputType.text,
-            floatingLabelBehavior: FloatingLabelBehavior.auto,
-            padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
-          ),
-          const SizedBox(height: 20),
-          CalendarField(
-            selectedDate: value.birthDayString,
-            withShadow: true,
-            isDateRange: false,
-            function: () => calendarDialog(
-              context,
-              DateTime.now(),
-              (p0) {
-                Navigator.of(context).pop();
-                value.setBirthDay(p0);
-              },
+      return SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/AQ_PRIME_LOGO_2.png',
+              height: 130,
+              width: double.infinity,
             ),
-          ),
-          const SizedBox(height: 20),
-          InputTextField(
-            controller: mobileNumber,
-            hintText: 'Mobile Number',
-            height: 55,
-            keyboardType: TextInputType.number,
-            floatingLabelBehavior: FloatingLabelBehavior.auto,
-            padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
-            prefixIconPadding: const EdgeInsets.only(top: 8, bottom: 10),
-            prefixIcon: GestureDetector(
-              onTap: () => countryCodeDialog(context: context),
-              child: Text(value.countryCode.dialCode.toString(), style: TextStyle(fontSize: 15, color: HexColor('#BEBBBB'), fontWeight: FontWeight.w700)),
+            const SizedBox(height: 25),
+            InputTextField(
+              controller: firstName,
+              hintText: 'First Name',
+              height: 55,
+              keyboardType: TextInputType.text,
+              floatingLabelBehavior: FloatingLabelBehavior.auto,
+              padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
             ),
-          ),
-          const SizedBox(height: 20),
-          InputTextField(
-            controller: password,
-            hintText: 'Password',
-            height: 55,
-            keyboardType: TextInputType.text,
-            obscureText: value.isObscurePassword,
-            floatingLabelBehavior: FloatingLabelBehavior.auto,
-            padding: const EdgeInsets.only(left: 60, top: 8, bottom: 8),
-            suffixIconPadding: const EdgeInsets.only(top: 3, bottom: 10, right: 10),
-            suffixIcon: IconButton(
-              onPressed: () => value.setIsObscurePassword(),
-              icon: Icon(
-                value.isObscurePassword ? Icons.visibility_off_outlined : Icons.visibility_outlined,
-                color: HexColor('#BEBBBB'),
-                size: 20,
+            const SizedBox(height: 20),
+            InputTextField(
+              controller: lastName,
+              hintText: 'Last Name',
+              height: 55,
+              keyboardType: TextInputType.text,
+              floatingLabelBehavior: FloatingLabelBehavior.auto,
+              padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
+            ),
+            const SizedBox(height: 20),
+            InputTextField(
+              controller: email,
+              hintText: 'E-mail',
+              height: 55,
+              keyboardType: TextInputType.text,
+              floatingLabelBehavior: FloatingLabelBehavior.auto,
+              padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
+            ),
+            const SizedBox(height: 20),
+            CalendarField(
+              selectedDate: value.birthDayString,
+              withShadow: true,
+              isDateRange: false,
+              function: () => calendarDialog(
+                context,
+                DateTime.now(),
+                (p0) {
+                  Navigator.of(context).pop();
+                  value.setBirthDay(p0);
+                },
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          InputTextField(
-            controller: confirmPassword,
-            hintText: 'Confirm Password',
-            height: 55,
-            keyboardType: TextInputType.text,
-            obscureText: value.isObscureConfirmPassword,
-            floatingLabelBehavior: FloatingLabelBehavior.auto,
-            padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
-            suffixIconPadding: const EdgeInsets.only(top: 3, bottom: 10, right: 10),
-            suffixIcon: IconButton(
-              onPressed: () => value.setIsObscureConfirmPassword(),
-              icon: Icon(
-                value.isObscureConfirmPassword ? Icons.visibility_off_outlined : Icons.visibility_outlined,
-                color: HexColor('#BEBBBB'),
-                size: 20,
+            const SizedBox(height: 20),
+            InputTextField(
+              controller: mobileNumber,
+              hintText: 'Mobile Number',
+              height: 55,
+              keyboardType: TextInputType.number,
+              floatingLabelBehavior: FloatingLabelBehavior.auto,
+              padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+              prefixIconPadding: const EdgeInsets.only(top: 8, bottom: 10),
+              prefixIcon: GestureDetector(
+                onTap: () => countryCodeDialog(context: context),
+                child: Text(value.countryCode.dialCode.toString(), style: TextStyle(fontSize: 15, color: HexColor('#BEBBBB'), fontWeight: FontWeight.w700)),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          PrimaryButton(
-            height: 50,
-            width: double.infinity,
-            label: 'Register',
-            action: () {
-              value.sendAPI(firstName.text, lastName.text, mobileNumber.text, email.text, password.text, confirmPassword.text);
-              registrationDialog(context);
-            },
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Already on AQ-Prime?',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.w400,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 13,
-                  color: Colors.white,
+            const SizedBox(height: 20),
+            InputTextField(
+              controller: password,
+              hintText: 'Password',
+              height: 55,
+              keyboardType: TextInputType.text,
+              obscureText: value.isObscurePassword,
+              floatingLabelBehavior: FloatingLabelBehavior.auto,
+              padding: const EdgeInsets.only(left: 60, top: 8, bottom: 8),
+              suffixIconPadding: const EdgeInsets.only(top: 3, bottom: 10, right: 10),
+              suffixIcon: IconButton(
+                onPressed: () => value.setIsObscurePassword(),
+                icon: Icon(
+                  value.isObscurePassword ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                  color: HexColor('#BEBBBB'),
+                  size: 20,
                 ),
               ),
-              const SizedBox(width: 5),
-              GestureDetector(
-                onTap: () => Navigator.of(context).pop(),
-                child: const Text(
-                  'Sign-in',
+            ),
+            const SizedBox(height: 20),
+            InputTextField(
+              controller: confirmPassword,
+              hintText: 'Confirm Password',
+              height: 55,
+              keyboardType: TextInputType.text,
+              obscureText: value.isObscureConfirmPassword,
+              floatingLabelBehavior: FloatingLabelBehavior.auto,
+              padding: const EdgeInsets.only(left: 60, top: 10, bottom: 10),
+              suffixIconPadding: const EdgeInsets.only(top: 3, bottom: 10, right: 10),
+              suffixIcon: IconButton(
+                onPressed: () => value.setIsObscureConfirmPassword(),
+                icon: Icon(
+                  value.isObscureConfirmPassword ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                  color: HexColor('#BEBBBB'),
+                  size: 20,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            CheckboxLabel(
+              label: 'Accept Terms and Condition',
+              value: value.isAcceptedTermsAndCondition,
+              onChanged: () => value.setTermsAndCondition(),
+            ),
+            const SizedBox(height: 20),
+            PrimaryButton(
+              isDisabled: !value.isAcceptedTermsAndCondition,
+              height: 50,
+              width: double.infinity,
+              label: 'Register',
+              action: () {
+                value.sendAPI(firstName.text, lastName.text, mobileNumber.text, email.text, password.text, confirmPassword.text);
+                registrationDialog(context);
+              },
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Already on AQ-Prime?',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w700,
+                    fontWeight: FontWeight.w400,
                     fontStyle: FontStyle.normal,
                     fontSize: 13,
-                    color: Colors.red,
+                    color: Colors.white,
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(width: 5),
+                GestureDetector(
+                  onTap: () => Navigator.of(context).pop(),
+                  child: const Text(
+                    'Sign-in',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 13,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       );
     });
   }
