9489582621ff5708b37287585c132ba0bf1d5688
Vijae Manlapaz <hardknockrabbit@gmail.com>
2022-05-29 01:53:06 +0800

    chore: clean up

--- lib/app/screens/registration_screen.dart
+++ lib/app/screens/registration_screen.dart
@@ -22,35 +22,37 @@ class RegistrationScreen extends StatelessWidget {
         ),
         extendBodyBehindAppBar: true,
         backgroundColor: Colors.transparent,
-        body: Center(
-          child: AnimationConfiguration.staggeredList(
-            position: 0,
-            child: AnimatedSwitcher(
-              duration: const Duration(milliseconds: 800),
-              layoutBuilder: (widget, animation) {
-                return FadeInAnimation(
-                  duration: const Duration(milliseconds: 800),
-                  child: widget!,
-                  delay: const Duration(milliseconds: 1000),
-                );
-              },
-              child: SingleChildScrollView(
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 500),
-                  height: 810,
-                  width: double.infinity,
-                  margin: const EdgeInsets.only(
-                      left: 20, right: 20, top: 40, bottom: 20),
-                  padding: const EdgeInsets.only(
-                      left: 20, right: 20, top: 20, bottom: 20),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
-                    ),
-                  ),
-                  child: RegistrationCard(),
+        body: AnimationConfiguration.staggeredList(
+          position: 0,
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 800),
+            layoutBuilder: (widget, animation) {
+              return FadeInAnimation(
+                duration: const Duration(milliseconds: 800),
+                child: widget!,
+                delay: const Duration(milliseconds: 1000),
+              );
+            },
+            child: SingleChildScrollView(
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                height: MediaQuery.of(context).size.height -
+                    (MediaQuery.of(context).padding.top + 52 + 32),
+                width: double.infinity,
+                margin: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  top: MediaQuery.of(context).padding.top + 52,
+                  // bottom: 16,
                 ),
+                padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+                child: RegistrationCard(),
               ),
             ),
           ),
