b55ae20faa82337fcd5a7e5ba711b6b0cfb41eba
Vijae Manlapaz <hardknockrabbit@gmail.com>
2022-05-29 17:22:51 +0800

    fix: Content Search

--- lib/app/providers/search_provider.dart
+++ lib/app/providers/search_provider.dart
@@ -1,31 +1,30 @@
+import 'package:aq_prime/device/utils/api_request.dart';
 import 'package:aq_prime/domain/entities/content.dart';
 import 'package:flutter/material.dart';
 
 class SearchProvider with ChangeNotifier {
-  bool _isSearching = false;
-  bool _isLoading = true;
-  final List<Content> _data = [];
-  final List<Content> _searchData = [];
+  bool _isSuccess = false;
+  bool _isLoading = false;
+  List<Content> _data = [];
 
-  bool get isSearching => _isSearching;
   bool get isLoading => _isLoading;
   List<Content> get data => _data;
-  List<Content> get searchData => _searchData;
-  searched(searchString) {
-    _searchData.clear();
-    for (var i = 0; i < _data.length; i++) {
-      if (_data[i].title!.toLowerCase().contains(searchString.toLowerCase())) {
-        _searchData.add(_data[i]);
-      }
-    }
-
-    notifyListeners();
-  }
-
-  reset() {
-    _isSearching = false;
+  Future<List<Content>> search(String searchString) async {
     _isLoading = true;
-    _searchData.clear();
+    Map response = await API().request(
+      requestType: RequestType.get,
+      endPoint: '/contents',
+      parameter: {
+        'filter[title]': searchString,
+      },
+    );
+    _isSuccess = response['success'];
+    _isLoading = false;
+    if (_isSuccess) {
+      _data =
+          (response['data'] as List).map((e) => Content.fromJson(e)).toList();
+    }
     notifyListeners();
+    return _data;
   }
 }
